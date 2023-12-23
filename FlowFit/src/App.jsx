e22f16ccad8e063725b20836b4c13b7ff268accb
Devesh Krishan <deveshkrishan2003@gmail.com>
2023-12-22 23:41:17 -0800

    created grid and updated welcome message

--- FlowFit/src/App.jsx
+++ FlowFit/src/App.jsx
@@ -1,5 +1,4 @@
 import { Grid, Col, Card, Text, Metric } from "@tremor/react";
-import { useState } from 'react'
 import NavBar from './components/NavBar.jsx'
 import './App.css'
 
@@ -7,50 +6,8 @@ function App() {
   return (
     <div className="flex flex-col gap-10">
       <NavBar></NavBar>
-      
-      <div>
-        <div className="grid grid-cols-3 grid-rows-4">
-
-          <div class="Welcome Message" className="col-span-2">
-            <Card className="bg-content-card">
-              <Metric>Good morning! 🌞 Welcome to a new day of fitness and wellness. Let's make it count! 💪 #MorningMotivation #FitnessGoals 🔥🔥🔥</Metric>
-            </Card>
-          </div>
-    
-          <div class="Heatmap" className="col-span-1">
-            <Card className="bg-content-card">
-              <Metric>Heatmap</Metric>
-            </Card>
-          </div>
-          
-          <div class="" className="col-span-2 ">
-            <Card className="bg-content-card">
-              <Metric>Weekly Calendar</Metric>
-            </Card>
-          </div>
-
-          <div class="" className="col-span-1">
-            <Card className="bg-content-card">
-              <Metric>Graph 1</Metric>
-            </Card>
-          </div>
-
-          <div class="Week" className="col-span-2">
-            <Card className="bg-content-card">
-              <Metric>Days of the Week</Metric>
-            </Card>
-          </div>
-
-          <div class="" className="col-span-1">
-            <Card className="bg-content-card">
-              <Metric>Graph 2</Metric>
-            </Card>
-          </div>
-
-        </div>
-
-      </div>
-
+      <WelcomeMessage/>
+      {/* <HeatMap/> */}
 
     </div>
   )
@@ -59,3 +16,12 @@ function App() {
 
 export default App;
 
+function WelcomeMessage(){
+  return (
+    <div className="griddy">
+      <Card id="welcome">
+        <Metric>Good morning! 🌞 Welcome to a new day of fitness and wellness. Let's make it count! 💪 #MorningMotivation #FitnessGoals 🔥🔥🔥</Metric>
+      </Card>
+    </div>
+  )
+}
